03e0663e15a24a2553d456fe7e94d87aa5e42c42
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2021-12-09 22:01:37 +0100

    commit

---
+++ lib/app/config/constants/app_constant.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AppConstant {
+  AppConstant._();
+
+  /// TODO : Gender Type
+  static const List genderType = ["male", "female", "Others"];
+
+  /// TODO : Screen Size
+  //static double screenWidth = Get.size.width;
+  //static double screenHeight = Get.size.height;
+  static double screenWidth = Get.width;
+  static double screenHeight = Get.height;
+
+  /// TODO : Page Index
+  static int pageIndex = 0;
+  static PageController controller = PageController();
+
+  /// TODO : Curves
+  static const Curve curve = Curves.linearToEaseOut;
+
+  /// TODO : Transitions
+  static const Transition transitionRoute = Transition.topLevel;
+
+  /// TODO : Durations
+  static const Duration durationRoute = Duration(milliseconds: 100);
+  static const Duration durationSplash = Duration(milliseconds: 1500);
+  static const Duration durationAnimation = Duration(milliseconds: 1000);
+  static const Duration durationCheckedBox = Duration(milliseconds: 500);
+  static const Duration durationGender = Duration(milliseconds: 250);
+  static const Duration duration = Duration(milliseconds: 1500);
+
+  /// TODO : BoxShadow
+  static BoxShadow boxShadow = BoxShadow(
+    //color: AppTheme.shadowColor.withOpacity(.1),
+    blurRadius: 10,
+    spreadRadius: .1,
+  );
+}
---
+++ lib/app/config/messages/app_message.dart
@@ -0,0 +1,71 @@
+class AppMessage {
+  AppMessage._();
+  static const String noDataFound = "No Data Found";
+
+  /// TODO : App Icons
+  static const String appLogo = "assets/images/logo.png";
+  static const String appIcon = "assets/images/app_icon.png";
+  static const String appIconRound = "assets/images/app_icon_round.png";
+  static const String placeHolder = "https://vue.pixelstrap.com/multikart/_nuxt/img/6.64e2e9c.jpg";
+  static const String facebookIcon = "assets/images/facebook.png";
+  static const String googleIcon = "assets/images/google.png";
+
+  /// TODO : Login View
+  static const String hintEmail = "Email";
+  static const String hintPassword = "Password";
+  static const String rememberMe = "Remember me";
+  static const String forgetPassword = "Forget Password";
+  static const String loginButtonText = "Login";
+  static const String accessText = "Access with";
+  static const String loginLabel = "Don't have an account?";
+  static const String loginTitle = "Sign up";
+
+  /// TODO : Register View
+  static const String hintFirstname = "Firstname";
+  static const String hintLastname = "Lastname";
+  static const String hintMiddlename = "Middlename ( optional ) ";
+  static const String hintPhone = "Phone";
+  static const String registerButtonText = "Create new account";
+  static const String registerLabel = "Already have an account?";
+  static const String registerTitle = "Sign in";
+
+  /// TODO : Initial View
+  static const String appTitle = "MultiKart";
+
+  /// TODO : Home View
+  static const String labelHome = "Home";
+  static const String appCurrency = r"$";
+  static const String searchHint = "Search...";
+  static const String welcomeTitle = "Welcome To Fashion";
+  static const String labelSearch = "Search";
+  //static const String bestSelling = "Best Sellers";
+  //static const String brandNew = "Brand New";
+
+  /// TODO : Categories View
+  static const String labelCategories = "Categories";
+
+  /// TODO : Settings View
+  static const String labelSettings = "Settings";
+  static const String editButton = "Edit Profile";
+  static const String preferencesLabel = "PREFERENCES";
+  static const String languageLabel = "Language";
+  static const String languageEN = "English";
+  static const String languageAR = "Arabic";
+  static const String languageFR = "French";
+  static const String darkMoeLabel = "Dark Mode";
+  static const String notificationsLabel = "Receive Notifications";
+  static const String helpLabel = "Help";
+  static const String logoutLabel = "Logout";
+  static const String deleteButton = "Delete Account";
+
+  /// TODO : Profile View
+  static const String labelProfile = "Account";
+  static const String obscuringCharacter = "●";
+  static const String saveProfile = "Save";
+
+  /// TODO : Shopping View
+  static const String shopTitle = "My Cart";
+
+  /// TODO : Favorite View
+  static const String favTitle = "WishList";
+}
