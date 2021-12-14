5f2509e56101c4a64277e0423b5b86ba2cd177e0
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2021-12-14 17:55:24 +0100

    commit

--- lib/app/config/themes/app_theme.dart
+++ lib/app/config/themes/app_theme.dart
@@ -6,21 +6,10 @@ class AppTheme {
   static const Color mainColor = Color(0xFFF0F0F0);
   static const Color backgroundColor_1 = Color(0xff254c71);
   static const Color backgroundColor_2 = Color(0xFF071930);
-  static const Color primaryBackColor = Color(0xFFFFFFFF);
   static const Color imageColor = Color(0xFFFFFFFF);
   static const Color transparentColor = Color(0x00000000);
-  static const Color shadowColor = Color(0x1F000000);
-  static const Color borderColor = Color(0x8A000000);
-
-  /// Icon Colors
-  static const Color primaryIconColor = Color(0xFFFFFFFF);
-  static const Color secondaryIconColor = Color(0xFF000000);
 
   /// Text Colors
   static const Color primaryTextColor = Color(0xFFFFFFFF);
   static const Color secondaryTextColor = Color(0xFF000000);
-
-  static ThemeData get themeData {
-    return ThemeData();
-  }
 }
