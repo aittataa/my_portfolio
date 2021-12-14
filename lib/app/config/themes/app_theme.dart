bb3a7f77310aa9050e63548f00aa2e591e015f3b
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2021-12-14 17:56:13 +0100

    commit

--- lib/app/config/themes/app_theme.dart
+++ lib/app/config/themes/app_theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppTheme {
   AppTheme._();
 
-  static const Color mainColor = Color(0xFFF0F0F0);
+  static const Color mainColor = Color(0xFFFFFFFF);
   static const Color backgroundColor_1 = Color(0xff254c71);
   static const Color backgroundColor_2 = Color(0xFF071930);
   static const Color imageColor = Color(0xFFFFFFFF);
