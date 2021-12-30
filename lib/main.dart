aa699095376346353f7b37415966db6e460c7f46
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2021-12-30 13:36:49 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:get/get.dart';
 import 'package:my_portfolio/app/config/functions/app_function.dart';
 import 'package:my_portfolio/app/config/messages/app_message.dart';
@@ -7,7 +6,6 @@ import 'package:my_portfolio/app/shared/splash.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  setUrlStrategy(PathUrlStrategy());
   AppFunction.configureDependencies();
   runApp(Portfolio());
 }
